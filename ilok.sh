c8b5286aed64524aae5b9917b74376443816a51f
Kekok55 <71831121+Kekok55@users.noreply.github.com>
2020-09-29 13:16:23 +0700

    Create ilok.sh

---
+++ ilok.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+ulang="y"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;m'
+while [ $ulang = "y" ]
+clear
+sleep 1
+toilet -f big -F gay By Kekok
+read -p "Tulis Nama Anda :" i
+     echo $cyan"Welcome" $i
+echo $yellow"Pilih Tools Nya"
+echo $white
+do
+echo $blue"( 1 ) Hack Fb"
+echo $white
+echo $blue"( 2 ) Stabilkan Jaringan"
+echo $white
+echo $blue"( 3 ) Hack No Hp"
+echo $white
+echo $blue"( 0 ) exit"
+echo $white
+read -p "Pilihan? :" pill
+if [ $pill = "1" ]
+then
+     echo "Memuat...."
+     sleep 4
+     clear
+     sleep 1
+toilet -f big -F gay Hack Fb
+     echo $blue"Hack Fb By Kekok"
+     echo $yellow
+read -p "Masukan Nama :" n
+     echo $cyan"Mencari $n Di Fb"
+     echo $white
+     sleep 5
+     echo $cyan"Berhasil Menemukan $n Di Fb"
+     echo $white
+     sleep 1
+     echo $cyan"Sedang Menghack $n"
+     sleep 2
+     echo $white"Please Wait"
+     sleep 6
+     echo $cyan"Berhasil Menghack $n"
+     echo $white
+  sleep 1
+elif [ $pill = "2" ]
+then
+     echo "Memuat.."
+     sleep 4
+     clear
+     sleep 2
+toilet -f big -F gay Kekok
+     echo $green"Stabilkan Jaringan By kekok"
+     echo $cyan
+     sleep 2
+ping -s1000 1.1.1.1
+     sleep 1
+elif [ $pill = "3" ]
+then
+     echo "Memuat...."
+     sleep 4
+     clear
+     sleep 1
+toilet -f big -F gay Hack No Hp
+     echo $blue"Hack No hp By Kekok"
+     echo $yellow
+read -p "Masukan No Hp :" nom
+     echo $cyan"Mencari $nom Operator"
+     echo $white
+     sleep 5
+     echo $cyan"Berhasil Menemukan $nom Di Operator"
+     echo $white
+     sleep 1
+     echo $cyan"Sedang Menghack $nom"
+     sleep 2
+     echo $white"Please Wait"
+     sleep 6
+     echo $cyan"Berhasil Menghack $nom"
+     echo $white
+  sleep 1
+elif [ $pill = "0" ]
+then
+     echo $yellow"Terimakasih,,Byyyy..."
+     echo "exit"
+     exit
+else
+     echo $red"ERORR BOSQUH: Imput yang kamu masukan salahh!"
+     sleep 4
+     echo $ulang
+  fi
+done
